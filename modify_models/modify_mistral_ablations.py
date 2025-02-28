4856f1e55a0c86f240985342151df63515b50403
Ahmed AbouElhamayed <afa55@cornell.edu>
2025-02-28 10:36:05 -0500

    Fix Mistral baselines

--- modify_models/modify_mistral_ablations.py
+++ modify_models/modify_mistral_ablations.py
@@ -151,6 +151,7 @@ class MistralAttentionExperimental(nn.Module):
     def forward(
         self,
         hidden_states: torch.Tensor,
+        position_embeddings: Tuple[torch.Tensor, torch.Tensor],
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
         past_key_value: Optional[Union[DynamicCache, PredictorDynamicCache]] = None,
@@ -183,7 +184,7 @@ class MistralAttentionExperimental(nn.Module):
         key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
         value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
 
-        cos, sin = self.rotary_emb(value_states, position_ids)
+        cos, sin = position_embeddings
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
         
         if use_cache:
--- modify_models/modify_mistral_baselines.py
+++ modify_models/modify_mistral_baselines.py
@@ -136,6 +136,7 @@ class MistralAttentionExperimental(nn.Module):
     def forward(
         self,
         hidden_states: torch.Tensor,
+        position_embeddings: Tuple[torch.Tensor, torch.Tensor],
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
         past_key_value: Optional[Union[DynamicCache, BaselineDynamicCache]] = None,
@@ -176,7 +177,7 @@ class MistralAttentionExperimental(nn.Module):
         else:
             h2o_importance_history = None
         # cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)  # AHMED: Modified this to use the newer version.
-        cos, sin = self.rotary_emb(value_states, position_ids)
+        cos, sin = position_embeddings
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
 
         if use_cache:
